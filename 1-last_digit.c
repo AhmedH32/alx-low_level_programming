db4ff61f139b1d65ecabb86516d8546699de23b3
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-07-28 21:37:11 +0000

    work

---
+++ 1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int nf;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	nf = n % 10;
+
+	if (nf == 0)
+	{
+	printf("Last digit of %d is %d and is 0", n, nf);
+	}
+	else if (nf > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5", n, nf);
+	}
+	else if (nf < 6 && nf != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0", n, nf);
+	}
+	return (0);
+}
