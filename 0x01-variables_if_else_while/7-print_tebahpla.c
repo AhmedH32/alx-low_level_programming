b8a05db1cf71802b6650814a2a79a89ba4810523
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-07-28 22:11:38 +0000

    work

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char chr = 122;
+	int i = 0;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(chr);
+
+		chr--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
