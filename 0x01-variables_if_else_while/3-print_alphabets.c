4c30db9d3662e426e2275a856d39ef25d64844a4
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-07-28 22:03:17 +0000

    work

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char chr = 97;
+	int i = 0;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(chr);
+
+		chr++;
+	}
+
+	chr = 65;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(chr);
+
+		chr++;
+	}
+	putchar('\n');
+
+	return (0);
+}
