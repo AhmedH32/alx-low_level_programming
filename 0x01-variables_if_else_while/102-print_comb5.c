9937f559e320692e4287fb6a9e3357a1e4d0405e
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-01 01:00:41 +0000

    work

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,25 +12,15 @@ int main(void)
 	int i, j, k, l;
 
 	j = k = l = 48;
-
 	for (i = 49; i < 58; i++)
 	{
-		putchar(l);
-		putchar(k);
-		putchar(' ');
-		putchar(j);
-		putchar(i);
+		putter(i, j, k, l);
 
 		if (i  == 57 && j == 57 && k == 56 && l == 57)
 		{
 			break;
 		}
-		if (i < 57 || j < 57 || k < 57 || l < 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-
+		puttercom(i, j, k, l);
 		if (i == 57)
 		{
 			i = 47;
@@ -57,8 +47,40 @@ int main(void)
 		}
 
 	}
-
 	putchar('\n');
-
 	return (0);
+}
+/**
+ * putter - prints each number using putchar
+ * @i: first number
+ * @j: second number
+ * @k: third number
+ * @l: fourth number
+ *
+ * Return: void noithing
+ */
+void putter(int i, int j, int k, int l)
+{
+	putchar(l);
+	putchar(k);
+	putchar(' ');
+	putchar(j);
+	putchar(i);
+}
+/**
+ * puttercom - prints comams and space
+ * @i: first number
+ * @j: second number
+ * @k: third number
+ * @l: fourth number
+ *
+ * Return: nothing
+ */
+void puttercom(int i, int j, int k, int l)
+{
+	if (i < 57 || j < 57 || k < 57 || l < 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
 }
