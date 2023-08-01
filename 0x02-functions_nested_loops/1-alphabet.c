40074aba922ca0565fa88eab94a241172865113a
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-01 18:26:45 +0000

    work

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphapet - prints the alphapet
+ *
+ * Return: void
+ */
+void print_alphapet(void)
+{
+	int i;
+
+	for(i = 0; i <26; i++)
+	{
+		_putchar(i + 97);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+void print_alphapet(void);
 int _putchar(char c);
 
 
