6b518cfe04e2b34fa223a66230081061c08f1a8f
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-01 18:33:09 +0000

    work

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphapet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(i + 97);
+		}
+		_putchar('\n');
+	}
+}
