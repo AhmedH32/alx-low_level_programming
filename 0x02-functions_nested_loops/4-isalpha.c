aa15d471e862fee1d634aed53e6c9404243412b2
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-01 19:09:37 +0000

    work

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether input is alphabet
+ * @c: input as int from 0 - 255
+ *
+ * Return: 1 if alphapet , 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
