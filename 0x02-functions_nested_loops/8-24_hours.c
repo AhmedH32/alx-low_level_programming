499faa46d0e403777d2faad72afe15477152254f
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-01 19:58:10 +0000

    work

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * jack_bauer - prints all possible times
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	char i, j, k, l;
+
+	i = j = k = l = 0;
+
+	while (1)
+	{
+		_putchar(l + 48);
+		_putchar(k + 48);
+		_putchar(':');
+		_putchar(j + 48);
+		_putchar(i + 48);
+		_putchar('\n');
+
+		i++;
+
+		if (i == 10)
+		{
+			j++;
+			i = 0;
+		}
+		if (j == 6)
+		{
+			k++;
+			j = 0;
+		}
+		if (k == 4)
+		{
+			l++;
+			k = 0;
+		}
+		if (l == 3)
+			break;
+	}
+}
