91d16be4393311239087dd85dae9ba67d0e8162b
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-02 23:52:39 +0000

    work

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
- 	unsigned long long i = 0, j = 1, k, tmp;
+	unsigned long i = 0, j = 1, k, tmp;
 
 	for (k = 0; k < 50; k++)
 	{
-		printf("%llu, ", i);
+		printf("%lu, ", i);
 		tmp = j;
 		j = i + j;
 		i = tmp;
