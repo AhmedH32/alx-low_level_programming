9c215227b3c63805214984cf61853ee68b26fdbe
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-02 23:33:42 +0000

    work

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+ 	int i = 0, j = 1, k, tmp;
 
+	for (k = 0; k < 50; k++)
+	{
+		printf("%d, ", i);
+		tmp = j;
+		j = i + j;
+		i = tmp;
+
+	}
+	printf("\n");
+
+	return (0);
+}
