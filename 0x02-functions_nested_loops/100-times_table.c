0e2d109b00c25712ecb7105d1242c512492a50b1
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-02 23:03:58 +0000

    work

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,45 +8,42 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-
-	if (n != 0 && n != 15)
+	for (i = 0; i < n + 1; i++)
 	{
-		for (i = 0; i < n + 1; i++)
+		if (n < 0 || n > 15)
+			break;
+		for (j = 0; j < n + 1; j++)
 		{
-			for (j = 0; j < n + 1; j++)
+			k = i * j;
+			if (j == 0)
 			{
-				k = i * j;
-				if (j == 0)
-				{
-					_putchar(k + 48);
-				}
-				else if (k < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + 48);
-				}
-				else if (k < 100)
-				{
-
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + 48);
-					_putchar((k % 10) + 48);
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar((k / 100) + 48);
-					_putchar(((k / 10) % 10) + 48);
-					_putchar(((k % 10) + 48);
-				}
-				if (j != n)
-					_putchar(',');
-				else
-					_putchar('\n');
+				_putchar(k + 48);
+			}
+			else if (k < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + 48);
+			}
+			else if (k < 100)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((k / 100) + 48);
+				_putchar(((k / 10) % 10) + 48);
+				_putchar(((k % 10) + 48);
+			}
+			if (j != n)
+				_putchar(',');
+			else
+				_putchar('\n');
 			}
 		}
-	}
 }
