1927e7cc285342f6a0d409259098c4b2062ceb3a
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-10 23:05:13 +0000

    work

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,12 +10,17 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size - (i + 1); j++)
-			printf(" ");
-		for (j = 0; j < i; j++)
-			printf("#");
+	if (size <= 0)
 		printf("\n");
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - (i + 1); j++)
+				printf(" ");
+			for (j = 0; j < i; j++)
+				printf("#");
+			printf("\n");
+		}
 	}
 }
