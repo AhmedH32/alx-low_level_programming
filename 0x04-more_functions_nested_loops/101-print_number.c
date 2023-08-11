3e971c8efa70fc6de9cd3fb5b418080026286de8
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-11 01:30:54 +0000

    work

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,18 @@
 #include "main.h"
 
 /**
- * main - entry
- *
+ * print_number - prints an integer
+ *@n: integer to be printed
  * Return: 0
  */
+void print_number(int n)
+{
+	_putchar((n % 10) + 48);
+
+	while (n > 10)
+	{
+		n /= 10;
+		_putchar((n % 10) + 48);
+	}
+	_putchar('\n');
+}
