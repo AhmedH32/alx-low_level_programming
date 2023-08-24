793fa0323dee55102d57227d7f2365ec418b3109
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-24 20:07:01 +0000

    work

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of letters to copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (*(src + i) != '/0')
+		i++;
+	if (n < i)
+		n = i;
+	for (j = 0; j < n; j++)
+	{
+		*(dest + j) = *(src + j);
+	}
+	return (dest);
+}
