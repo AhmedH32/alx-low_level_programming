76169980947433722666ad48d6801c9926af7811
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-24 20:18:09 +0000

    work

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, j;
 
-	while (*(src + i) != '\0')
-		i++;
-	if (n < i)
-		n = i;
-	for (j = 0; j < n; j++)
-	{
-		*(dest + j) = *(src + j);
-	}
-
-	*(dest + j) = '\0';
-	return (dest);
+        while (*(src + i) != '\0')
+                i++;
+        if (n > i)
+                n = i;
+        for (j = 0; j < n; j++)
+        {
+                *(dest + j) = *(src + j);
+        }
+       *(dest + j) = '\0';
+        return (dest);	
 }
