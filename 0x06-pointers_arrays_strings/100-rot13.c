7d9443fa4c79f8076584281e1785e1db84b9fa6d
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-24 22:49:20 +0000

    work

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,15 +8,18 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char *s1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *s2 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *s2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == s1[j])
+			{
 				s[i] = s2[j];
+				break;
+			}
 		}
 	}
 	return (s);
