f78de1ba326baaf9e65ad229642daad9f9805c88
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-27 16:44:24 +0000

    work

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+void print_buffer(char *b, int size)
+{
+    int i = 0, j = 0, k, l, n;
+    
+    for (l = 0; l < size; )
+    {
+        printf("%08x: ",j);
+    
+        for(k = 0; k < 10; k++)
+        {
+            if (l >= size)
+            {
+                for (n = 0; n < 10- k; n++)
+                {
+                    printf("  ");
+                    if (n % 2)
+                        printf(" ");
+                }
+               
+                    break;
+            }
+                printf("%02x",b[i]);
+            if(k % 2)
+                printf(" ");
+            i++;
+            l++;
+        }
+        if (l >= size)
+            {
+                l -= k;
+                i -= k;
+            }
+        else
+        {
+            l -= 10;
+            i -= 10;
+        }
+        
+        for(k = 0; k < 10; k++)
+        {
+            if (l >= size)
+            {
+                    break;
+            }
+            if (b[i - 1] == '0')
+            {
+                printf(" ");
+                    break;
+            }
+            if (b[i] < ' ')
+                printf(".");
+            else
+                printf("%c",b[i]);
+            i++;
+            l++;
+        }
+        printf("\n");
+        j += 10;
+    }
+}
