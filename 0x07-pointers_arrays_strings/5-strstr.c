7b1223a242fc46877705b73815f6812bd20c7a7a
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-28 22:08:17 +0000

    work

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,42 +1,42 @@
 #include "main.h"
 
 /**
- * _strstr - returns a poointer to the beginning of a located substring
- * @haystack: stringt to searchi in
- * @needle: substring to find
- * Return: pointer to the beginning of the string
- */
+* _strstr - returns a poointer to the beginning of a located substring
+* @haystack: stringt to searchi in
+* @needle: substring to find
+* Return: pointer to the beginning of the string
+*/
 char *_strstr(char *haystack, char *needle)
 {
-    int i = 0, j = 0, k, l, flag = 0;
-    
-    while (haystack[i] != '\0')
-        i++;
-    while (needle[j] != '\0')
-        j++;
-    
-    for (k = 0; k < i && !flag && j > 0; k++)
-    {
-        if (haystack[k] == needle[0])
-        {
-            flag = 1;
-            for (l = 1; l < j && k + l < i; l++)
-            {
-                if (haystack[k + l] != needle[l])
-                {
-                    flag = 0;
-                    break;
-                }
-            }
-        }
-    }
-   if (j == 0)
-	   return (haystack);
-    if (flag)
-    {
-        k--;
-        return (haystack + k );
-    }
-    else
-        return (NULL);
+	int i = 0, j = 0, k, l, flag = 0;
+
+	while (haystack[i] != '\0')
+		i++;
+	while (needle[j] != '\0')
+		j++;
+
+	for (k = 0; k < i && !flag && j > 0; k++)
+	{
+		if (haystack[k] == needle[0])
+		{
+			flag = 1;
+			for (l = 1; l < j && k + l < i; l++)
+			{
+				if (haystack[k + l] != needle[l])
+				{
+					flag = 0;
+					break;
+				}
+			}
+		}
+	}
+	if (j == 0)
+		return (haystack);
+	if (flag)
+	{
+		k--;
+		return (haystack + k);
+	}
+	else
+	return (NULL);
 }
