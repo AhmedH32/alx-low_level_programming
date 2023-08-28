eb968d07e6e75d81e3fa10b14f245c1c36af75f6
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-28 20:26:58 +0000

    work

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * _memset - sets number of bytes of array to value
+ * @s: pointer to char array
+ * @b: the char
+ * @n: number of bytes
+ * Return: pointer to array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		s[j] = b;
+	}
+	return (s);
+}
