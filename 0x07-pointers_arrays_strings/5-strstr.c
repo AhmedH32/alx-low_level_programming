e5aef32ed7da3234bbce8677c22b2d3c4893f83c
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-28 21:51:37 +0000

    work

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,35 +1,41 @@
 #include "main.h"
 
 /**
-* _strpbrk - pointer to first char from accept to appear
-* @s: pointer to string to search
-* @accept: charset
-* Return: pointer to unsigned int
-*/
-char *_strpbrk(char *s, char *accept)
+ * _strstr - returns a poointer to the beginning of a located substring
+ * @haystack: stringt to searchi in
+ * @needle: substring to find
+ * Return: pointer to the beginning of the string
+ */
+char *_strstr(char *haystack, char *needle)
 {
-	int i = 0, j = 0, k, l, found = 1;
-
-	while (s[i] != '\0')
-		i++;
-	while (accept[j] != '\0')
-		j++;
-	for (k = 0; k < i; k++)
-	{
-		found = 1;
-		for (l = 0; l < j; l++)
-		{
-			if (s[k] != accept[l])
-			{
-				found = 0;
-				break;
-			}
-		}
-		if (found)
-			break;
-	}
-	if (found)
-		return (s + k);
-	else
-		return (NULL);
+    int i = 0, j = 0, k, l, flag = 0;
+    
+    while (haystack[i] != '\0')
+        i++;
+    while (needle[j] != '\0')
+        j++;
+    
+    for (k = 0; k < i && !flag; k++)
+    {
+        if (haystack[k] == needle[0])
+        {
+            flag = 1;
+            for (l = 1; l < j && k + l < i; l++)
+            {
+                if (haystack[k + l] != needle[l])
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+    }
+   
+    if (flag)
+    {
+        k--;
+        return (haystack + k );
+    }
+    else
+        return (NULL);
 }
