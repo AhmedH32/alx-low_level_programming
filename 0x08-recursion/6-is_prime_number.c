f6b591a9c0c5288d40ab99d3235fe7687dc1499e
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-30 21:29:36 +0000

    work

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * actual_prime - the actual fnction responsible for prime checking
+ * @n: the number to check
+ * @j: counter
+ * Return: 0 if not prime 1 if prime
+ */
 int actual_prime(int n, int j)
 {
 	if (j < n)
@@ -14,9 +20,14 @@ int actual_prime(int n, int j)
 	else
 		return (0);
 }
+/**
+ * is_prime_number - set up function to check if prime
+ * @n: number to check
+ * Return: 1 if prime 0 if not
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return(actual_prime(n, 2));
+	return (actual_prime(n, 2));
 }
