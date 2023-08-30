0aa0bf8dbbfc149cdb347480955d6c36d77e0230
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-30 21:12:24 +0000

    work

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,17 +2,19 @@
 
 int actual_prime(int n, int j)
 {
-	    if (j < n)
-		            if (n % j == 0)
-				                return (0);
-	            else
-			                return (actual_prime(n, j + 1));
-	        if (n == j)
-			        return (1);
+	if (j < n)
+	{
+		if (n % j == 0)
+			return (0);
+		else
+			return (actual_prime(n, j + 1));
+	}
+	if (n == j)
+		return (1);
 }
 int is_prime_number(int n)
 {
-	    if (n <= 1)
-		            return (0);
-	        return(actual_prime(n, 2));
+	if (n <= 1)
+		return (0);
+	return(actual_prime(n, 2));
 }
