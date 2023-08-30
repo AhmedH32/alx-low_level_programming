8268d24f5c8be6a97d4307869a8f89b7fb56d9c7
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-08-30 21:51:04 +0000

    work

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -19,6 +19,18 @@ int is_actual_palindrome(char *s, int len, int j)
 
 }
 /**
+ * _strlen - gets length of string
+ * @s: pointer to the string
+ * Return: length of str
+ */
+int _strlen(char *s)
+{
+	if (*s)
+		return (1 + _strlen(s + 1));
+	else
+		return (0);
+}
+/**
  * is_palindrome - check if a string is a palindrome
  * @s: pointer to the string
  * Return: 1 if palindrome 0 if not
@@ -27,6 +39,6 @@ int is_palindrome(char *s)
 {
 	int i = 0;
 
-	i = _strlen_recursion(s);
+	i = _strlen(s);
 	return (is_actual_palindrome(s, i - 1, 0));
 }
