a3c543cf8ccdedb68add14d7cbf489ee3d159c4b
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-01 23:58:56 +0000

    work

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of inputs
+ * @argv: pointer to an array of pointers ot strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", *argv);
+	return (0);
+}
