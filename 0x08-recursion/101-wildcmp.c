31870457332d6a507688f74ef7b853aa6c5c9ea4
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-01 19:30:27 +0000

    work

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,72 +1,28 @@
 #include "main.h"
 
-int actual_wildcmp(char *s1, char *s2, int len1, int len2, int i, int j)
-{   
-    if (i == len1 || j == len2)
-    {
-        if (s1[i] == s2[j])
-        {
-            return (1);
-        }
-        else if (s2[j]== '*' && j + 1 == len2)
-        {
-            return (actual_wildcmp(s1, s2, len1, len2, i - 1, j));
-        }
-	else
-        {
-            return (0);
-        }
-    }
-    if (s1[i] == s2[j])
-    {
-        return (actual_wildcmp(s1, s2, len1, len2, i + 1, j + 1));
-    }
-    else if (s2[j] == '*')
-    {
-        if (s2[j + 1] == '*')
-        {
-            return (actual_wildcmp(s1, s2, len1, len2, i, j + 1));
-        }
-        else if (j + 1 == len2)
-        {
-            return (1);
-        }
-        else
-        {
-            if (i >= len1)
-                return (0);
-            if (s1[i] == s2[j + 1])
-            {
-                return (actual_wildcmp(s1, s2, len1, len2, i + 1, j + 2));
-            }
-            else
-            {
-                return (actual_wildcmp(s1, s2, len1, len2, i + 1, j));
-            }
-        }
-    }
-    else
-    {
-        return (0);
-    }
-}
-/**
- * _strlen - gets length of string
- * @s: pointer to the string
- * Return: length of str
- */
-int _strlen(char *s)
+
+int wildwild(char *str1, char *str2)
 {
-        if (*s)
-                return (1 + _strlen(s + 1));
-        else
-                return (0);
+	if (*str1 == '\0')
+	{
+		if (*str2 != '\0' && *str2 == '*')
+		{
+			return (wildwild(str1, str2 + 1));
+		}
+		return (*str2 == '\0');
+	}
+
+	if (*str2 == '*')
+	{
+		return (wildwild(str1, str2 + 1) || wildwild(str1 + 1, str2));
+	}
+	else if (*str1 == *str2)
+	{
+		return (wildwild(str1 + 1, str2 + 1));
+	}
+	return (0);
 }
 int wildcmp(char *s1, char *s2)
 {
-    int i, j;
-    
-    i = _strlen(s1);
-    j = _strlen(s2);
-    return (actual_wildcmp(s1, s2, i, j, 0, 0));
+	return wildwild(s1, s2);
 }
