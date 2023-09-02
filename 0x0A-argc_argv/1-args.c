c65370094198cba3e055fd4883da81bdb6cff7ab
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-02 00:17:19 +0000

    work

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of inputs
+ * @argv: pointer to array of pointers
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
