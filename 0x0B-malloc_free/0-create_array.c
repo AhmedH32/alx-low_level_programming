f6031e0871ea12efe251a889cffedcb1bfc838bf
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-05 01:03:35 +0000

    work

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	int i;
 
 	chrarr = malloc(sizeof(char) * size);
-	if (chrarr == null)
+	if (chrarr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
