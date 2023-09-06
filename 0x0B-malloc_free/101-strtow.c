24b577a6841631826f5602f8b54f3bf5fec795f1
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-06 00:38:27 +0000

    work

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strtow - seperates a string into a 2d array of different words
+ * @str: the string itself
+ * Return: a pointer to a pointer
+ */
+char **strtow(char *str)
+{
+	int len = 0;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	while (str[i])
+		len++;
+
+
+
+
+}
