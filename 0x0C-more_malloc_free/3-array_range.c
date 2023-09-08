1331acec9737c0a188aed2e44c6a8b53853c5fbe
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-08 00:10:03 +0000

    work

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,10 +12,10 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) * (max - min + 1));
+	arr = malloc(sizeof(*arr) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; min <= max; min++, i++)
 		arr[i] = min;
-	return (min);
+	return (arr);
 }
