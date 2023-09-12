7325982ab2361ade17725afc06d87cea5bc7f302
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-12 23:31:14 +0000

    work

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ * Return: returns pointer to new instance of dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog = malloc(sizeof(dog_t));
+
+	if (newdog == NULL)
+		return (NULL)
+	newdog->name = name;
+	newdog->age = age;
+	newdog->owner = owner;
+	return (newdog);
+}
