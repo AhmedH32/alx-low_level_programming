fa9f888650ab68debb286528dce70bdbeea572fa
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-12 01:55:07 +0000

    work

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-
-struct dog 
+/**
+ * struct dog - this is the struct for a dog
+ * @name: its name
+ * @age: its age
+ * @owner: read
+ */
+struct dog
 {
 	char *name;
 	float age;
