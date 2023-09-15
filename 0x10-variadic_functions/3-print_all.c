9ed9f90fc2e54a028663c0cafc35453070e945c5
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-15 22:52:59 +0000

    work

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,7 +7,7 @@
  */
 void print_all(const char * const format, ...)
 {
-	int actlen, uselen, i = 0;
+	int i = 0;
 	va_list ap;
 	char *str;
 
@@ -19,25 +19,28 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(ap, char));
+				printf("%c", va_arg(ap, int));
 				break;
 			case 'i':
 				printf("%i", va_arg(ap, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(ap, float));
+				printf("%f", va_arg(ap, double));
 				break;
 			case 's':
 				str = va_arg(ap, char*);
-				if (str == NULL)
+				while (str == NULL)
 					str = "(nil)";
 				printf("%s", str);
 				break;
 			default:
-				break;
+				i++;
+				continue;
 		}
-		while (format[i + 1] != '\0')
+		if (format[i + 1] != '\0')
+		{
 			printf(", ");
+		}
 		i++;
 	}
 	printf("\n");
