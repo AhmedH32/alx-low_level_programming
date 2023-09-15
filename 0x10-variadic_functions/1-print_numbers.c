fb7161dbdb46f112a70e00c6aa6d7d475b6c955d
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-09-15 00:04:20 +0000

    work

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,14 +8,14 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int i;
+	unsigned int i;
 	va_list ap;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (i != n - 1 && seperator != NULL)
+		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 
 	}
