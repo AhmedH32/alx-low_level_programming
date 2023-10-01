d4c851caa861881d9a9d05aa6a21c2b3f83f7c4b
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-10-01 16:44:37 +0000

    work

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct listint_s - struct for a linked list
+ * @n: int data
+ * @next: link to next node
+ */
+typedef struct listint_s
+{
+	int  n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+
+
+#endif
