1d67cbc75b77a1f731f35c94226cf95fc75ec466
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2023-10-01 12:33:08 +0000

    work

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: pointer to head node
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *cur = head, tmp;
+
+	if (head = NULL)
+		return;
+	while (cur != NULL)
+	{
+		tmp = cur->next;
+		free(cur);
+		cur = tmp;
+	}
+
+
+}
